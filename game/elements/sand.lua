b744a1cfe44be3d3f6107cc0f0a073e45add8a32
JameDevOfficial <jame.bayerl@gmail.com>
2025-06-23 21:16:12 +0200

    fixed minor issues in sand logic

--- game/elements/sand.lua
+++ game/elements/sand.lua
@@ -5,18 +5,23 @@ local replaceElements = {"empty", "water"}
 
 function M.sandCalculation(grid, gridFactor, x, y)
     for i, v in ipairs(replaceElements) do
-        if grid[y][x] == element and grid[y + 1][x] == v then --sand floating
+        if grid[y][x] ~= element then return end
+
+        if y < gridFactor and grid[y + 1][x] == v then --sand floating
             grid[y + 1][x] = element
             grid[y][x] = v
+            return
         end
 
-        if grid[y][x] == element and grid[y + 1][x] == element then --double sand stack
-            if grid[y + 1][x + 1] == v then               --diagonal right
+        if y < gridFactor and grid[y + 1][x] == element then --double sand stack
+            if x < gridFactor and grid[y + 1][x + 1] == v then               --diagonal right
                 grid[y][x] = v
                 grid[y + 1][x + 1] = element
-            elseif grid[y + 1][x - 1] == v then --diagonal left
+                return 
+            elseif x > 1 and grid[y + 1][x - 1] == v then --diagonal left
                 grid[y][x] = v
                 grid[y + 1][x - 1] = element
+                return 
             end
         end
     end
