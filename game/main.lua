760dc8a3388a1eb59301fafe885ae9395359cd42
JameDevOfficial <jame.bayerl@gmail.com>
2025-07-23 22:01:42 +0200

    changed button color, started working on multi line element buttons (with test elements)

--- game/main.lua
+++ game/main.lua
@@ -15,7 +15,7 @@ Grid = {}
 
 BLACK = { 0, 0, 0, 1 }
 CurrentMode = "sand"
-Buttons = { "empty", "sand", "water", "wall" }
+Buttons = { "empty", "sand", "water", "wall", "plant", "test", "Test", "TTest", "TTTETS" }
 Regular = love.graphics.newFont("fonts/Rubik-Regular.ttf")
 Medium = love.graphics.newFont("fonts/Rubik-Medium.ttf")
 Light = love.graphics.newFont("fonts/Rubik-Light.ttf")
@@ -30,6 +30,17 @@ MovedGrid = {}
 ButtonWidth = 90
 ButtonHeight = 30
 ButtonPadding = 10
+local labelStyle = {
+    normal  = { bg = { 0.25, 0.25, 0.25 }, fg = { 0.1, 0.1, 0.1 } },
+    hovered = { bg = { 0.19, 0.6, 0.73 }, fg = { 1, 1, 1 } },
+    active  = { bg = { 1, 0.6, 0 }, fg = { 1, 1, 1 } }
+}
+
+local buttonStyle = {
+    normal  = { bg = { 0.25, 0.25, 0.25 }, fg = { 0.73, 0.73, 0.73 } },
+    hovered = { bg = { 0.19, 0.6, 0.73 }, fg = { 1, 1, 1 } },
+    active  = { bg = { 1, 0.6, 0 }, fg = { 1, 1, 1 } }
+}
 
 local screenWidth, screenHeight, minSize
 local padding = 0.
@@ -154,11 +165,21 @@ function love.keypressed(k)
 end
 
 local function drawUi()
-    local totalButtonRowWidth = ButtonWidth * #Buttons + ButtonPadding * (#Buttons - 1)
+    local availableWidth = minSize - 300
+    local buttonsPerRow = math.floor((availableWidth + ButtonPadding) / (ButtonWidth + ButtonPadding))
+    if buttonsPerRow < 1 then buttonsPerRow = 1 end
     --Element Buttons
-    suit.layout:reset(((screenWidth - minSize) / 2) + ((minSize - totalButtonRowWidth) / 2), 0, ButtonPadding)
+    suit.layout:reset(
+        ((screenWidth - minSize) / 2) +
+        ((minSize - (buttonsPerRow * ButtonWidth + (buttonsPerRow - 1) * ButtonPadding)) / 2),
+        0, ButtonPadding)
     suit.theme.color.normal.fg = { 0.73, 0.73, 0.73 }
+    local RowCount = 0
     for i, v in ipairs(Buttons) do
+        if RowCount > 0 and RowCount % buttonsPerRow == 0 then
+            
+        end
+        RowCount = RowCount + 1
         if suit.Button(v, suit.layout:col(ButtonWidth, ButtonHeight)).hit then
             CurrentMode = v
         end
