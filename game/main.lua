6cce64d225d68f7fd88c4df9b9f300084f654b95
JameDevOfficial <jame.bayerl@gmail.com>
2025-07-24 20:01:13 +0200

    made the element buttons automatically center and start a new row when full

--- game/main.lua
+++ game/main.lua
@@ -30,17 +30,6 @@ MovedGrid = {}
 ButtonWidth = 90
 ButtonHeight = 30
 ButtonPadding = 10
-local labelStyle = {
-    normal  = { bg = { 0.25, 0.25, 0.25 }, fg = { 0.1, 0.1, 0.1 } },
-    hovered = { bg = { 0.19, 0.6, 0.73 }, fg = { 1, 1, 1 } },
-    active  = { bg = { 1, 0.6, 0 }, fg = { 1, 1, 1 } }
-}
-
-local buttonStyle = {
-    normal  = { bg = { 0.25, 0.25, 0.25 }, fg = { 0.73, 0.73, 0.73 } },
-    hovered = { bg = { 0.19, 0.6, 0.73 }, fg = { 1, 1, 1 } },
-    active  = { bg = { 1, 0.6, 0 }, fg = { 1, 1, 1 } }
-}
 
 local screenWidth, screenHeight, minSize
 local padding = 0.
@@ -91,17 +80,7 @@ local drawGrid = function(emptyAll)
     end
 end
 
-function love.resize()
-    screenWidth, screenHeight = love.graphics.getDimensions()
-    minSize = (screenHeight < screenWidth) and screenHeight or screenWidth
-    cellSize = {
-        x = ((minSize - padding * (GridFactor + 1)) / GridFactor),
-        y = ((minSize - padding * (GridFactor + 1)) / GridFactor)
-    }
-    xCenter = math.floor(screenWidth / 2)
-    xStart = xCenter - (GridFactor / 2) * cellSize.x
-    drawGrid()
-end
+
 
 local function getGridElementAtCursor(mx, my)
     for y = 1, GridFactor + 1 do
@@ -138,11 +117,58 @@ local function drawAtCursor()
     end
 end
 
+local function drawUi()
+    local availableWidth = minSize - 300
+    local buttonsPerRow = math.floor((availableWidth + ButtonPadding) / (ButtonWidth + ButtonPadding))
+    if buttonsPerRow < 1 then buttonsPerRow = 1 end
+
+    --Element Buttons
+    local buttonCount = 0
+    local rows = 0
+
+    suit.layout:reset(
+        ((screenWidth - minSize) / 2) +
+        ((minSize - (buttonsPerRow * ButtonWidth + (buttonsPerRow - 1) * ButtonPadding)) / 2),
+        0, ButtonPadding)
+
+    for i, v in ipairs(Buttons) do
+        if buttonCount > 0 and buttonCount % buttonsPerRow == 0 then
+            rows = rows + 1
+            suit.layout:reset(
+                ((screenWidth - minSize) / 2) +
+                ((minSize - (buttonsPerRow * ButtonWidth + (buttonsPerRow - 1) * ButtonPadding)) / 2),
+                rows * (ButtonHeight + ButtonPadding), ButtonPadding)
+        end
+        buttonCount = buttonCount + 1
+        if suit.Button(v, suit.layout:col(ButtonWidth, ButtonHeight)).hit then
+            CurrentMode = v
+        end
+    end
+
+    --Text Labels
+    suit.layout:reset((screenWidth - minSize) / 2, 0, ButtonPadding)
+    suit.Label(performance.getFPS(), { align = "left" }, suit.layout:row(200, 30))
+    suit.layout:reset((screenWidth - minSize) / 2, 0, ButtonPadding)
+    suit.Label("Current Mode: " .. CurrentMode, { align = "right" }, suit.layout:row(minSize, 30))
+    if not DEBUG then return end
+    suit.Label(debugInfo, { align = "left" }, suit.layout:row())
+end
+
+--Load
 function love.load()
     drawGrid(true)
     love.graphics.setFont(Regular)
+    suit.theme.color.normal.fg = { 0, 0, 0 }
 end
 
+--Draw
+function love.draw()
+    drawGrid()
+    drawUi()
+    suit.draw()
+end
+
+--Update
 function love.update(dt)
     if IsPaused then return end
     performance.addEntry(dt)
@@ -160,42 +186,20 @@ function love.update(dt)
     lib.wait(waitTime)
 end
 
+--Keypressed
 function love.keypressed(k)
     debug.keypressed(k, GridFactor, Grid)
 end
 
-local function drawUi()
-    local availableWidth = minSize - 300
-    local buttonsPerRow = math.floor((availableWidth + ButtonPadding) / (ButtonWidth + ButtonPadding))
-    if buttonsPerRow < 1 then buttonsPerRow = 1 end
-    --Element Buttons
-    suit.layout:reset(
-        ((screenWidth - minSize) / 2) +
-        ((minSize - (buttonsPerRow * ButtonWidth + (buttonsPerRow - 1) * ButtonPadding)) / 2),
-        0, ButtonPadding)
-    suit.theme.color.normal.fg = { 0.73, 0.73, 0.73 }
-    local RowCount = 0
-    for i, v in ipairs(Buttons) do
-        if RowCount > 0 and RowCount % buttonsPerRow == 0 then
-            
-        end
-        RowCount = RowCount + 1
-        if suit.Button(v, suit.layout:col(ButtonWidth, ButtonHeight)).hit then
-            CurrentMode = v
-        end
-    end
-    --Text Labels
-    suit.theme.color.normal.fg = { 0, 0, 0 }
-    suit.layout:reset((screenWidth - minSize) / 2, 0, ButtonPadding)
-    suit.Label(performance.getFPS(), { align = "left" }, suit.layout:row(200, 30))
-    suit.layout:reset((screenWidth - minSize) / 2, 0, ButtonPadding)
-    suit.Label("Current Mode: " .. CurrentMode, { align = "right" }, suit.layout:row(minSize, 30))
-    if not DEBUG then return end
-    suit.Label(debugInfo, { align = "left" }, suit.layout:row())
-end
-
-function love.draw()
+--Resized
+function love.resize()
+    screenWidth, screenHeight = love.graphics.getDimensions()
+    minSize = (screenHeight < screenWidth) and screenHeight or screenWidth
+    cellSize = {
+        x = ((minSize - padding * (GridFactor + 1)) / GridFactor),
+        y = ((minSize - padding * (GridFactor + 1)) / GridFactor)
+    }
+    xCenter = math.floor(screenWidth / 2)
+    xStart = xCenter - (GridFactor / 2) * cellSize.x
     drawGrid()
-    drawUi()
-    suit.draw()
 end
